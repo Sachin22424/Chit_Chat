6f352a2cb712335dc452c8bcaef589b3c3e887f2
Sachin Maurya <sachin22424@iiitd.ac.in>
2024-07-07 15:21:58 +0530

    Frontend Update

---
+++ client_frontend/src/context/ChatContext.jsx
@@ -0,0 +1,37 @@
+import { createContext, useState, useEffect } from "react";
+import { baseURL, getRequest } from "../utils/services";
+
+export const ChatContext = createContext();
+
+export const ChatContextProvider = ({ children, user }) => {
+    const [userChats, setUserChats] = useState(null);
+    const [isUserChatsLoading, setIsUserChatsLoading] = useState(false);
+    const [userChatsError, setUserChatsError] = useState(null);
+
+    useEffect(() => {
+        const getUserChats = async () => {
+            if (user?._id) {
+                setIsUserChatsLoading(true);
+                setUserChatsError(null);
+
+                const response = await getRequest(`${baseURL}/chat/${user._id}`);
+
+                setIsUserChatsLoading(false);
+
+                if (response.error) {
+                    setUserChatsError(response.message);
+                } else {
+                    setUserChats(response);
+                }
+            }
+        };
+
+        getUserChats();
+    }, [user]);
+
+    return (
+        <ChatContext.Provider value={{ userChats, setUserChats, isUserChatsLoading, setIsUserChatsLoading, userChatsError, setUserChatsError }}>
+            {children}
+        </ChatContext.Provider>
+    );
+};
